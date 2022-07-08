2c84dae5b147aa1e24fa11e9ac8fb2f06f208385
yamato_hagi <la.luce.ymt0326@gmail.com>
2022-07-08 22:31:45 +0900

    Initial Commit

---
+++ PropupImagePicker/PropupImagePickerApp.swift
@@ -0,0 +1,17 @@
+//
+//  PropupImagePickerApp.swift
+//  PropupImagePicker
+//
+//  Created by yamato hagi on 2022/07/08.
+//
+
+import SwiftUI
+
+@main
+struct PropupImagePickerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}

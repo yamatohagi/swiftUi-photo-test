05ca168f9678a900cd7cb5fcc7e1b15b4277c46c
yamato_hagi <la.luce.ymt0326@gmail.com>
2022-07-31 16:33:22 +0900

    とりあえず

--- PropupImagePicker/View/SlideImagePickerView.swift
+++ PropupImagePicker/View/SlideImagePickerView.swift
@@ -12,13 +12,15 @@ import Photos
 struct SlideImagePickerView: View {
     
     @StateObject var imagePickerModel = ImagePickerViewModel()
-    @State  var currentIndex = 0
+
     @GestureState private var dragOffset: CGFloat = 0
     
     let itemPadding: CGFloat = 30
     
-    var body: some View {
 
+    
+    var body: some View {
+  
         Button("更新") {
             self.imagePickerModel.update()
     
@@ -27,54 +29,75 @@ struct SlideImagePickerView: View {
             self.imagePickerModel.delete()
     
         }
+        
         GeometryReader { bodyView in
             LazyHStack(spacing: itemPadding) {
                 
                 let fetchedImagesAry = $imagePickerModel.fetchedImages
                 
+                
+                
+                
+                
+  
                 ForEach(fetchedImagesAry) { $imageAsset in
                     
 //                    let _ = print("fetchedImagesAry.count\(fetchedImagesAry.count)")
-          
 
+          
+           
                     // カルーセル対象のView
                     SlideContent(imageAsset: imageAsset)
                         .offset(x: self.dragOffset)
-                        .offset(x: -CGFloat(self.currentIndex) * (bodyView.size.width * 1 + itemPadding))
+                        .offset(x: -CGFloat(self.imagePickerModel.currentAry.index(of: self.imagePickerModel.currentKey)!) * (bodyView.size.width * 1 + itemPadding))
                         .gesture(
                             DragGesture()
                                 .updating(self.$dragOffset, body: { (value, state, _) in
                                     // 先頭・末尾ではスクロールする必要がないので、画面サイズの1/5までドラッグで制御する
-                                    if self.currentIndex == 0, value.translation.width > 0 {
+                                    if self.imagePickerModel.currentAry.index(of: self.imagePickerModel.currentKey)! == 0, value.translation.width > 0 {
                                         state = value.translation.width / 5
 //                                        let _ = print(self.dragOffset)
-                                    } else if self.currentIndex == (fetchedImagesAry.count - 1), value.translation.width < 0 {
+                                    } else if self.imagePickerModel.currentAry.index(of: self.imagePickerModel.currentKey)! == (fetchedImagesAry.count - 1), value.translation.width < 0 {
                                         state = value.translation.width / 5
                                     } else {
                                         state = value.translation.width
                                     }
                                 })
                                 .onEnded({ value in
-                                    var newIndex = self.currentIndex
+                                    var newIndex = self.imagePickerModel.currentAry.index(of: self.imagePickerModel.currentKey)!
                                     // ドラッグ幅からページングを判定
+                                    
+                                    
+                                        var now_ary_index =  self.imagePickerModel.currentAry.index(of: self.imagePickerModel.currentKey)!
+                                        
+                                   
+                                         
                                     if abs(value.translation.width) > bodyView.size.width * 0.1 {
-                                        newIndex = value.translation.width > 0 ? self.currentIndex - 1 : self.currentIndex + 1
+                                        newIndex = value.translation.width > 0 ? now_ary_index - 1 : now_ary_index + 1
+                              
                                     }
                                     // 最小ページ、最大ページを超えないようチェック
                                     if newIndex < 0 {
                                         newIndex = 0
                                     } else if newIndex > (fetchedImagesAry.count - 1) {
                                         newIndex = fetchedImagesAry.count - 1
-                                    }
                                     
-                                    self.currentIndex = newIndex
+                                     
+                                    }
+                                    self.imagePickerModel.currentKey = self.imagePickerModel.currentAry[newIndex]
+                                    
+                                    self.imagePickerModel.currentIndex = self.imagePickerModel.currentAry.index(of: self.imagePickerModel.currentKey)!
+                                    let _ = print("self.imagePickerModel.currentIndex\(self.imagePickerModel.currentIndex)")
+                                    let _ = print(self.imagePickerModel.currentKey)
+                                    let _ = print(self.imagePickerModel.currentAry.index(of: self.imagePickerModel.currentKey))
+                       
                                 })
                         )
                         .onAppear() {
                             if imageAsset.thumbnail == nil{
                                 // MARK: Fetching Thumbnail Image
                                 let manager = PHCachingImageManager.default()
-                                manager.requestImage(for: imageAsset.asset, targetSize: CGSize(width: 300, height: 300), contentMode: .aspectFill, options: nil) { image,
+                                manager.requestImage(for: imageAsset.asset, targetSize: PHImageManagerMaximumSize, contentMode: .aspectFill, options: nil) { image,
                                     _ in
                                     imageAsset.thumbnail = image
                                 }
@@ -96,7 +119,7 @@ struct SlideImagePickerView: View {
     func SlideContent(imageAsset: ImageAsset)->some View{
         GeometryReader{ proxy in
             let size = proxy.size
-            
+
             if let thumbnail = imageAsset.thumbnail{
                 Image(uiImage: thumbnail)
                     .resizable()
--- PropupImagePicker/ViewModel/ImagePickerViewModel.swift
+++ PropupImagePicker/ViewModel/ImagePickerViewModel.swift
@@ -14,27 +14,31 @@ class ImagePickerViewModel: NSObject, ObservableObject {
     // MARL: Properties
     @Published var fetchedImages: [ImageAsset] = []
     @Published var selectedImages: [ImageAsset] = []
+    @Published var currentIndex:Int = 0
+    @Published var currentKey:String = ""
+    @Published var currentAry:[String] = []
     
-    @Published var currentIndex:Int
     let beforeAry: [ImageAsset] = []
+    
     var fetchResult: PHFetchResult<PHAsset>? = nil
     
     override init() {
-        self.currentIndex = 0
+
         super.init()
         let beforeAry = fetchImages()
         self.fetchedImages = beforeAry
         PHPhotoLibrary.shared().register(self)
         
+        
     }
     
     
     public func update(){
-        
-        fetchedImages = []
-        fetchImages()
-        print("あああaaaaaaaaaーーーーーーーーーーーーー")
-        print(fetchedImages.count)
+        self.currentIndex = 1
+//        fetchedImages = []
+//        fetchImages()
+//        print("あああaaaaaaaaaーーーーーーーーーーーーー")
+//        print(fetchedImages.count)
     }
     
     public func delete(){
@@ -50,18 +54,46 @@ class ImagePickerViewModel: NSObject, ObservableObject {
         var ary:[ImageAsset] = []
         let options = PHFetchOptions()
         // MARK: Modify As Per Your Wish
-        options.fetchLimit = 100
-        options.includeHiddenAssets = true
+        options.fetchLimit = 1
+        options.includeHiddenAssets = false
         options.includeAssetSourceTypes = [.typeUserLibrary]
-        options.sortDescriptors = [NSSortDescriptor(key: "creationDate", ascending: false)]
+        options.sortDescriptors = [NSSortDescriptor(key: "creationDate", ascending: true)]
+
         self.fetchResult = PHAsset.fetchAssets(with: .image,options: options)
         
         
-        self.fetchResult?.enumerateObjects{ asset, _, _ in
+        
+        
+        self.fetchResult?.enumerateObjects{ asset, index, _ in
             
-            let imageAssset: ImageAsset = .init(asset: asset)
+       
+       
+            var imageAssset: ImageAsset = .init(asset: asset)
+            let options = PHImageRequestOptions()
+            
+      
+//            if imageAssset.thumbnail == nil{
+//                // MARK: Fetching Thumbnail Image
+//                let manager = PHCachingImageManager.default()
+//                manager.requestImage(for: imageAssset.asset, targetSize: PHImageManagerMaximumSize, contentMode: .aspectFit, options: options) { image,
+//                    _ in
+//                    imageAssset.thumbnail = image
+//                }
+//            }
+            
+            if(self.currentKey == ""){
+                self.currentKey = asset.value(forKey: "filename") as! String
+            }
+           
+            self.currentAry.append(asset.value(forKey: "filename") as! String)
             ary.append(imageAssset)
+            
+            print("aaa変更前")
+               let _ = print(self.currentAry)
         }
+        
+        
+        
         return ary
         //こっちでは消せる
         //        var removeList: [ImageAsset] = []
@@ -98,13 +130,32 @@ extension ImagePickerViewModel: PHPhotoLibraryChangeObserver {
                     
                     var indexChenged = 0
                     if let removed = changeDetails.removedIndexes, !removed.isEmpty {
-                        removed.map({ IndexPath(item: $0, section: 0) })
-                        print(removed.map({ IndexPath(item: $0, section: 0) }))
+                       
+                        
+                        removed.map({
+                            print("$0----\($0)")
+                            print("self.currentAry\(self.currentAry)")
+//                            self.currentAry.remove(at: $0)
+             })
+                        
 //                        collectionView.deleteItems(at: removed.map({ IndexPath(item: $0, section: 0) }))
                     }
+                    for deletion in changeDetails.removedObjects {
+                        if deletion.value(forKey: "filename") as! String == self.currentKey {
+                            self.currentIndex = self.currentIndex + 1
+                            self.currentKey = self.currentAry[self.currentIndex]
+                            print("これね変更後のcurrentKey\(self.currentKey)")
+                        }
+                        
+                    }
+            
+                    
                     if let inserted = changeDetails.insertedIndexes, !inserted.isEmpty {
 //                        collectionView.insertItems(at: inserted.map({ IndexPath(item: $0, section: 0) }))
-                        print(inserted.map({ IndexPath(item: $0, section: 0) }))
+                        inserted.map({ i in
+                            print(i)
+//                            self.currentAry.insert($0.id, at: $0)
+                         })
                     }
                     changeDetails.enumerateMoves { fromIndex, toIndex in
 //                        collectionView.moveItem(at: IndexPath(item: fromIndex, section: 0),
@@ -129,22 +180,26 @@ extension ImagePickerViewModel: PHPhotoLibraryChangeObserver {
                     //                        print((removed))
                     //                    }
                 }
-                
+                self.currentAry = []
                 let newFetchResul = changeDetails.fetchResultAfterChanges
-                newFetchResul.enumerateObjects{ asset, _, _ in
+                newFetchResul.enumerateObjects{ asset, index, _ in
                     
                     let imageAssset: ImageAsset = .init(asset: asset)
+                    self.currentAry.append(asset.value(forKey: "filename") as! String)
+  
                     ary.append(imageAssset)
                 }
                 
-                
-                for i in (0 ..< ary.count){
-                    PHCachingImageManager.default().requestImage(for: ary[i].asset, targetSize: CGSize(width: 300, height: 300), contentMode: .aspectFill, options: nil) { image,
-                        _ in
-                        ary[i].thumbnail = image
-                    }
-                }
+//
+//                for i in (0 ..< ary.count){
+//                    PHCachingImageManager.default().requestImage(for: ary[i].asset, targetSize: CGSize(width: 300, height: 300), contentMode: .aspectFill, options: nil) { image,
+//                        _ in
+//                        ary[i].thumbnail = image
+//                    }
+//                }
                 self.fetchedImages = ary
+                print("aaa変更後")
+                let _ = print(self.currentAry)
          
             }
         })
